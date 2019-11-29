cde1bb7d8976686f01abd3cb67f0930b46d895fe
John <johnopana2016@gmail.com>
2019-11-29 16:22:55 +0300

    highlight. quote. directive

---
+++ src/app/quote-details/highlight.quote.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlighteQuoteDirective } from './highlighte-quote.directive';
+
+describe('HighlighteQuoteDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlighteQuoteDirective();
+    expect(directive).toBeTruthy();
+  });
+});
