cf41db9b2ac7aa64046bfef9a3bb8344da2f8c48
John <johnopana2016@gmail.com>
2019-11-29 16:03:26 +0300

    added routing .ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,29 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { FormComponent } from './form/form.component';
-import { PipeComponent } from './pipe/pipe.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteComponent } from './quote/quote.component';
+import { HighlighteQuoteDirective } from './highlighte-quote.directive';
+import { TimeCountPipe } from './time-count.pipe';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    QuotesComponent,
-    FormComponent,
-    PipeComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    QuoteComponent,
+    HighlighteQuoteDirective,
+    TimeCountPipe,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
