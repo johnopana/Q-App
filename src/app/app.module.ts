53d1a932145daa634a0c32c6f27d4f4769d1d1a4
John <johnopana2016@gmail.com>
2019-11-29 14:36:51 +0300

    addedd app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { FormComponent } from './form/form.component';
+import { PipeComponent } from './pipe/pipe.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuotesComponent,
+    FormComponent,
+    PipeComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
