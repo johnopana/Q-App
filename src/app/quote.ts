31c84e61c389fa93a8d8ed77a1a1c4f855b279fd
John <johnopana2016@gmail.com>
2019-11-30 14:17:33 +0300

    added thumb upp

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,7 +2,4 @@ export class Quote {
   public quote: string;
   public publisher: string;
   public author: string;
-  public upvote: number;
-  public downvote: number;
-  public myDate: Date;
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,10 +8,25 @@ import {Quote} from '../quote'
 })
 export class QuotesComponent implements OnInit {
 
+quotes : Quote [] = [
 
+{ publisher:"John Opana",
+quote : `I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don\'t deserve me at my best.`,
+author :"Marilyn Monroe"
+},
+{
+publisher :"John Opana",
+quote :`Be yourself; everyone else is already taken. `,
+author :"Oscar Wilde"
+},
+{
+  publisher :"John Opana",
+  quote :`Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.`,
+  author :"Albert Einstein"
+}
+]
 
-  constructor() { }
-
+constructor(){}
   ngOnInit() {
   }
 
