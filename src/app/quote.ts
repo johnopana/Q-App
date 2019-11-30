202b27eccfd2a09de4b033efcb4db7bfa0009cd6
John <johnopana2016@gmail.com>
2019-11-30 13:37:05 +0300

    added quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,8 @@
-
 export class Quote {
-// public quote:string;
-// public publisher: string;
-// public author: string;
-// public upvote: number;
-// public downvote: number;
-// public myDate:Date;
+  public quote: string;
+  public publisher: string;
+  public author: string;
+  public upvote: number;
+  public downvote: number;
+  public myDate: Date;
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+
 
   constructor() { }
 
