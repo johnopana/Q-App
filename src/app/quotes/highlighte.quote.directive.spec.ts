fda6e70d602a059641e7838cac64c43862d0546c
John <johnopana2016@gmail.com>
2019-12-05 16:38:02 +0300

    Auto-generated commit

--- src/app/quotes/highlighte.quote.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-// import { HighlighteQuoteDirective } from './highlighte.quote.directive';
-
-// describe('HighlighteQuoteDirective', () => {
-//   it('should create an instance', () => {
-//     const directive = new HighlighteQuoteDirective();
-//     expect(directive).toBeTruthy();
-//   });
-// });
